c8c2f6ce2a3016e2e71e6a439c9c2f814ad5fc1f
theusaf <theusafyt@gmail.com>
2020-05-22 10:01:32 -0700

    Update examples

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,19 +1,38 @@
 from kahoot import client
-pin = 3076110
+pin = 7612977
 bot = client()
-bot.loggingMode = True
-bot.join(pin,"foobar")
+# bot.loggingMode = True
+bot.join(pin,"KahootPY",["a","b","c"])
 def joined():
     print("joined")
 bot.on("ready",joined)
 def badname():
-    print("leaving")
+    print("invalid name")
     bot.leave()
 bot.on("invalidName",badname)
-def answer():
-    bot.answer(0)
+def answer(q):
+    print("Question started. Answering '0'")
+    q.answer(0)
 bot.on("questionStart",answer)
-def qs():
-    print("question started")
-    pass
-bot.on("quizStart",qs)
+def qs(q):
+    print("Quiz Started: " + q.name)
+bot.on("quiz",qs)
+def question(q):
+    print("Question of type " + q.type + " got!")
+bot.on("question",question)
+def rank(qe):
+    print("Question Ended. In rank " + qe.rank + " with " + qe.total + " points.")
+bot.on("questionEnd",rank)
+def end():
+    print("disconnected")
+bot.on("disconnect",end)
+def sub():
+    print("answer submitted")
+bot.on("questionSubmit",sub)
+def finishtext(fin):
+    print("finished with " + fin.metal + " medal")
+bot.on("finishText",finishtext)
+def done(dat):
+    print("quiz finished")
+    print(dat)
+bot.on("finish",done)
